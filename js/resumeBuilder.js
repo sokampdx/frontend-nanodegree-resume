abe85b01324f04785848545c864f63652beb239e
Anthony RTK <sokasoka@hotmail.com>
2017-09-18 16:51:56 -0700

    added all four object placeholders

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,40 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$("#main").append("Anthony So"); 
+var name = "Anthony So";
+var role = "Software Engineer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+//$("#main").
+
+var bio = {
+  "name"    : name,
+  "role"    : role,
+  "contact" : "contact info",
+  "pic_url" : "http://fillmurray.com/200/300", 
+  "welcome" : "Welcome",
+  "skill"   : "skillz"
+}
+
+var work = {
+  "position"  : "current position",
+  "employer"  : "employer!",
+  "year"      : "2017"
+}
+
+var education = {
+  "school"  : "School",
+  "year"    : "2011"
+}
+
+var projects = {
+  "project" : "some project"
+}
+
+//$("#header").prepend(formattedRole).prepend(formattedName);
+
+//$("#main").append(bio.name).append(bio.role).append(bio.contact).append(bio.pic_url);
+//$("#main").append(work.position).append(education.school);
+
